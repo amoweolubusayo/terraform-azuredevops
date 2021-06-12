801ffbc4d8b1dc21bf6f0b716ccbf43697d37910
amoweolubusayo <amoweolubusayo@gmail.com>
2021-06-12 18:43:22 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  subscription_id = var.subscriptionId
+  tenant_id       = var.tenantId
+  client_id       = var.clientId
+  client_secret   = var.clientSecret
+  features { }
+}
